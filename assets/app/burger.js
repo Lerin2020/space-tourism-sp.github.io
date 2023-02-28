e11350bff3e9adc64d0b5046c60233916469d4e7
Lerin2020 <66265675+Lerin2020@users.noreply.github.com>
2023-02-28 22:18:39 +0100

    Add files via upload

--- assets/app/burger.js
+++ assets/app/burger.js
@@ -3,3 +3,20 @@ document.querySelector('.burger')
     document.querySelector('.burger').classList.toggle('active')
     document.querySelector('.nav-list').classList.toggle('active')
 })
+console.log('scroll')
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 && !(document.querySelector('.burger').classList.contains('active'))){
+        document.querySelector('.navigation').style.opacity = '0'
+    }else {
+        document.querySelector('.navigation').style.opacity = ''
+    }
+})
+window.addEventListener('scroll', () => {
+    if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && document.querySelector('.burger').classList.contains('active')) {
+        document.querySelector('#logo').style.visibility = 'hidden'
+      } else if ((document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) && !(document.querySelector('.burger').classList.contains('active'))) {
+        document.querySelector('#logo').style.visibility = 'visible'
+      }else {
+        document.querySelector('#logo').style.visibility = 'visible'
+      }
+})
