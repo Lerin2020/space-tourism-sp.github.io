8ea767774fc4d3f80365400fa065172642f3f9f8
Lerin2020 <66265675+Lerin2020@users.noreply.github.com>
2023-03-01 21:04:52 +0100

    Add files via upload

--- assets/app/crew.js
+++ assets/app/crew.js
@@ -1,49 +1,89 @@
-const buttons = document.querySelectorAll('.button')
-const buttonList = [...buttons]
-const image = document.querySelector('.crew-image')
-const title = document.querySelector('.title')
-const fullName = document.querySelector('.full')
-const description = document.querySelector('.description')
+const buttons = document.querySelectorAll(".button");
+const buttonList = [...buttons];
+const image = document.querySelector(".crew-image");
+const title = document.querySelector(".title");
+const fullName = document.querySelector(".full");
+const description = document.querySelector(".description");
+window.addEventListener("DOMContentLoaded", () => {
+  description.style.opacity = "1";
+});
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    Animation();
+  });
+});
+
+function Animation() {
+  setTimeout(() => {
+    description.style.opacity = "0";
+    description.style.transform = 'translateY(2vw)'
+    image.style.opacity = "0";
+    image.style.transform = 'translateY(2vw)'
+    fullName.style.opacity = "0"
+    fullName.style.transform = 'translateY(2vw)'
+    title.style.opacity = "0";
+    title.style.transform = 'translateY(2vw)'
+  }, 0);
+  setTimeout(() => {
+    description.style.opacity = "1";
+    description.style.transform = ''
+    image.style.transform = ''
+    image.style.opacity = '1'
+    image.style.transform = ''
+    fullName.style.opacity ='1'
+    fullName.style.transform = ''
+    title.style.opacity = '1'
+    title.style.transform = ''
+  }, 500);
+}
 buttons.forEach((button, index) => {
-    button.addEventListener('click', (e) => {
-        updateSelection(index, e) 
-        if (buttonList[0].classList.contains('active')){         
-                image.src = './assets/crew/image-douglas-hurley.png' 
-                title.textContent  =   "Commander"   
-                fullName.textContent ="Douglas Hurley"
-                description.textContent ="Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
-        }else if (buttonList[1].classList.contains('active')) {   
-                image.src = './assets/crew/image-mark-shuttleworth.png'  
-                title.textContent  =     "Mission Specialist" 
-                fullName.textContent ="Mark Shuttleworth"
-                description.textContent = "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
-        } else if(buttonList[2].classList.contains('active')) {      
-                image.src = './assets/crew/image-victor-glover.png'
-                title.textContent  =    "Pilot"  
-                fullName.textContent ="Victor Glover"
-                description.textContent = "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer."
-        }
-         else if(buttonList[3].classList.contains('active')) {
-                image.src = './assets/crew/image-anousheh-ansari.png'
-                title.textContent  =    "Flight Engineer"   
-                fullName.textContent ="Anousheh Ansari"
-                description.textContent ="Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space."
-        }else {
-            image.src = './assets/crew/image-douglas-hurley.png'
-        }
-    })
-})
+  button.addEventListener("click", (e) => {
+    updateSelection(index, e);
+    if (buttonList[0].classList.contains("active")) {
+      setTimeout(() => {
+        image.src = "./assets/crew/image-douglas-hurley.png";
+        title.textContent = "Commander";
+        fullName.textContent = "Douglas Hurley";
+        description.textContent =
+          "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.";
+      }, 250);
+    } else if (buttonList[1].classList.contains("active")) {
+      setTimeout(() => {
+        image.src = "./assets/crew/image-mark-shuttleworth.png";
+        title.textContent = "Mission Specialist";
+        fullName.textContent = "Mark Shuttleworth";
+        description.textContent =
+          "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.";
+      }, 250);
+    } else if (buttonList[2].classList.contains("active")) {
+      setTimeout(() => {
+        image.src = "./assets/crew/image-victor-glover.png";
+        title.textContent = "Pilot";
+        fullName.textContent = "Victor Glover";
+        description.textContent =
+          "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.";
+      }, 250);
+    } else if (buttonList[3].classList.contains("active")) {
+      setTimeout(() => {
+        image.src = "./assets/crew/image-anousheh-ansari.png";
+        title.textContent = "Flight Engineer";
+        fullName.textContent = "Anousheh Ansari";
+        description.textContent =
+          "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.";
+      }, 250);
+    } else {
+      image.src = "./assets/crew/image-douglas-hurley.png";
+    }
+  });
+});
 
 function updateSelection(index, e) {
-    buttons.forEach((button, i) => {
-        
-        if(button.classList.contains('active')){
-            button.classList.remove('active')
-            e.target.classList.add('active')
-        }else {
-            button.classList.remove('active')
-        }
-        
-    })
-    
+  buttons.forEach((button, i) => {
+    if (button.classList.contains("active")) {
+      button.classList.remove("active");
+      e.target.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 }
--- assets/app/technology.js
+++ assets/app/technology.js
@@ -4,7 +4,26 @@ const image = document.querySelector('.tech-image')
 const header = document.querySelector('p.header')
 const info = document.querySelector('p.info')
 const media = window.matchMedia('(max-width: 768px)')
-
+buttons.forEach((link) => {
+    link.addEventListener("click", () => {
+      Animation();
+    });
+  });
+  
+  function Animation() {
+    setTimeout(() => {
+      header.style.opacity = "0";
+      header.style.transform = 'translateY(2vw)'
+       info.style.opacity = "0";
+      info.style.transform = 'translateY(2vw)'
+    }, 0);
+    setTimeout(() => {
+      header.style.opacity = "1";
+      header.style.transform = ''
+      info.style.opacity = '1'
+      info.style.transform = ''
+    }, 500);
+  }
 function handleScreen(e) {
     if(e.matches) {
         if(buttonsList[0].classList.contains('chosen')){
@@ -22,16 +41,25 @@ function handleScreen(e) {
         
                 if(buttonsList[0].classList.contains('chosen')) {
                     image.src = "./assets/technology/image-launch-vehicle-landscape.jpg"
-                    header.textContent = "Launch vehicle"
-                    info.textContent =  "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+                    setTimeout(() => {
+                        header.textContent = "Launch vehicle"
+                        info.textContent =  "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+                    },250)
+                    
                 }else if(buttonsList[1].classList.contains('chosen')) {
                     image.src = "./assets/technology/image-spaceport-landscape.jpg"
-                    header.textContent = "Spaceport"
-                    info.textContent = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+                    setTimeout(() => {
+                        header.textContent = "Spaceport"
+                        info.textContent = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+                    },250)
+                    
                 }else if(buttonsList[2].classList.contains('chosen')) {
                     image.src = "./assets/technology/image-space-capsule-landscape.jpg"
-                    header.textContent = "Space capsule"
-                    info.textContent = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+                    setTimeout(() => {
+                        header.textContent = "Space capsule"
+                        info.textContent = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+                    },250)
+                    
                 }else {
                     image.src = "./assets/technology/image-launch-vehicle-landscape.jpg"
                     header.textContent = ''
@@ -55,16 +83,25 @@ function handleScreen(e) {
             
                     if(buttonsList[0].classList.contains('chosen')) {
                         image.src = "./assets/technology/image-launch-vehicle-portrait.jpg"
-                        header.textContent = "Launch vehicle"
-                        info.textContent =  "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+                        setTimeout(() => {
+                            header.textContent = "Launch vehicle"
+                            info.textContent =  "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+                        },250)
+                        
                     }else if(buttonsList[1].classList.contains('chosen')) {
                         image.src = "./assets/technology/image-spaceport-portrait.jpg"
-                        header.textContent = "Spaceport"
-                        info.textContent = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+                        setTimeout(() => {
+                            header.textContent = "Spaceport"
+                            info.textContent = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+                        },250)
+                        
                     }else if(buttonsList[2].classList.contains('chosen')) {
                         image.src = "./assets/technology/image-space-capsule-portrait.jpg"
-                        header.textContent = "Space capsule"
-                        info.textContent = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+                        setTimeout(() => {
+                            header.textContent = "Space capsule"
+                            info.textContent = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+                        },250)
+                        
                     }else {
                         image.src = "./assets/technology/image-launch-vehicle-portrait.jpg"
                         header.textContent = ''
