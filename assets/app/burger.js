1b80c4d5673b9ecad2ad4cc5bb161976b2037562
Lerin2020 <66265675+Lerin2020@users.noreply.github.com>
2023-03-01 18:11:58 +0100

    Add files via upload

--- assets/app/burger.js
+++ assets/app/burger.js
@@ -3,18 +3,21 @@ document.querySelector('.burger')
     document.querySelector('.burger').classList.toggle('active')
     document.querySelector('.nav-list').classList.toggle('active')
 })
-console.log('scroll')
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 50 || window.pageYOffset > 50 && !(document.querySelector('.burger').classList.contains('active'))){
+    if(window.pageYOffset > 1 && !(document.querySelector('.burger').classList.contains('active'))){
         document.querySelector('.navigation').style.opacity = '0'
+        console.log('not contains')
+    }else if(window.pageYOffset > 1 && (document.querySelector('.burger').classList.contains('active'))) {
+        document.querySelector('.navigation').style.opacity = '1'
+        console.log('contains')
     }else {
-        document.querySelector('.navigation').style.opacity = ''
+      document.querySelector('.navigation').style.opacity = '1'
     }
 })
 window.addEventListener('scroll', () => {
-    if ((window.pageYOffset > 50 || window.pageYOffset > 50) && document.querySelector('.burger').classList.contains('active')) {
+    if ((window.pageYOffset > 1) && document.querySelector('.burger').classList.contains('active')) {
         document.querySelector('#logo').style.visibility = 'hidden'
-      } else if ((window.pageYOffset < 50 || window.pageYOffset < 50) && !(document.querySelector('.burger').classList.contains('active'))) {
+      } else if ((window.pageYOffset < 1) && !(document.querySelector('.burger').classList.contains('active'))) {
         document.querySelector('#logo').style.visibility = 'visible'
       }else {
         document.querySelector('#logo').style.visibility = 'visible'
