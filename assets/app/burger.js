f3ccf588d4b24cb9df16a0774869728c1672946d
Lerin2020 <66265675+Lerin2020@users.noreply.github.com>
2023-03-01 21:04:13 +0100

    Add files via upload

--- assets/app/burger.js
+++ assets/app/burger.js
@@ -3,23 +3,3 @@ document.querySelector('.burger')
     document.querySelector('.burger').classList.toggle('active')
     document.querySelector('.nav-list').classList.toggle('active')
 })
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 1 && !(document.querySelector('.burger').classList.contains('active'))){
-        document.querySelector('.navigation').style.opacity = '0'
-        console.log('not contains')
-    }else if(window.pageYOffset > 1 && (document.querySelector('.burger').classList.contains('active'))) {
-        document.querySelector('.navigation').style.opacity = '1'
-        console.log('contains')
-    }else {
-      document.querySelector('.navigation').style.opacity = '1'
-    }
-})
-window.addEventListener('scroll', () => {
-    if ((window.pageYOffset > 1) && document.querySelector('.burger').classList.contains('active')) {
-        document.querySelector('#logo').style.visibility = 'hidden'
-      } else if ((window.pageYOffset < 1) && !(document.querySelector('.burger').classList.contains('active'))) {
-        document.querySelector('#logo').style.visibility = 'visible'
-      }else {
-        document.querySelector('#logo').style.visibility = 'visible'
-      }
-})
